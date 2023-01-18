5685d796c7bee2ec38d2c6442f9835e7ec5fa2e9
Vikleo <vikleoboy@gmail.com>
2023-01-18 19:38:04 +0530

    a little changes

--- client/src/pages/product.js
+++ client/src/pages/product.js
@@ -13,56 +13,56 @@ import AdjustIcon from '@mui/icons-material/Adjust';
 export const Product = () => {
   return (
     <>
-    <div>product</div>
-    <div className='rectangle'>
-    <div className='arroww'><ArrowBackIosIcon /><ArrowForwardIosIcon />
-    <span className='text-col'> Navigation</span>
-    <div className='internal-rect'>
-      <div className='text-col  row-item-flex'> <PostAddIcon /><AddCommentOutlinedIcon/>
-      <DriveFileRenameOutlineIcon/><LockOutlinedIcon /></div>
-      
-     
-    </div>
-    
-    </div>
-    </div>
-     <div>
-    <div className='Rect-box col-item-flex'>
-      <div className='blue-col'><CheckBoxIcon />
-    <span className='text-col'> Puma Mens Deviate </span>
-    {/* <div className='green-col'>
+      <div>product</div>
+      <div className="rectangle">
+        <div className="arroww">
+          <ArrowBackIosIcon />
+          <ArrowForwardIosIcon />
+          <span className="text-col"> Navigation</span>
+          <div className="internal-rect">
+            <div className="text-col  row-item-flex">
+              {' '}
+              <PostAddIcon />
+              <AddCommentOutlinedIcon />
+              <DriveFileRenameOutlineIcon />
+              <LockOutlinedIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="Rect-box col-item-flex">
+          <div className="blue-col">
+            <CheckBoxIcon />
+            <span className="text-col"> Puma Mens Deviate </span>
+            {/* <div className='green-col'>
       <AdjustIcon /><span>online</span>
 
     </div>
    */}
-    
-    
-    </div>
-    <div className='blue-col'><CheckBoxIcon />
-    <span className='text-col'> Puma Mens Deviate </span></div>
-    <div className='blue-col'><CheckBoxIcon />
-    <span className='text-col'> Puma Mens Deviate </span></div>
-    <div className='blue-col'><CheckBoxIcon />
-    <span className='text-col'> Puma Mens Deviate </span></div>
-    <div className='blue-col'><CheckBoxIcon />
-    <span className='text-col'> Puma Mens Deviate </span></div>
-    <div className='blue-col'><CheckBoxIcon />
-    <span className='text-col'> Puma Mens Deviate </span></div>
-
-  </div>
-   
-  
-
-
-
-
-
-
-    
-  </div>
-
-
-
+          </div>
+          <div className="blue-col">
+            <CheckBoxIcon />
+            <span className="text-col"> Puma Mens Deviate </span>
+          </div>
+          <div className="blue-col">
+            <CheckBoxIcon />
+            <span className="text-col"> Puma Mens Deviate </span>
+          </div>
+          <div className="blue-col">
+            <CheckBoxIcon />
+            <span className="text-col"> Puma Mens Deviate </span>
+          </div>
+          <div className="blue-col">
+            <CheckBoxIcon />
+            <span className="text-col"> Puma Mens Deviate </span>
+          </div>
+          <div className="blue-col">
+            <CheckBoxIcon />
+            <span className="text-col"> Puma Mens Deviate </span>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
